277344777da565cdd22d2970d853346c92d6b6b9
chris-hildebrandt <ch.hild15@gmail.com>
2022-08-21 23:53:40 -0600

    project presentable

--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -1,48 +1,35 @@
 <template>
   <div class="profile-page">
     <div class="container-fluid ">
-      <div class="row">
+      <div class="row text-white text-shadow">
         <div v-if="profile" class="card cover-img">
           <div class="card-body text-start">
             <div>
-              <p class="">Name:{{profile.name}}</p>
+              <p class="">Name:{{ profile.name }}</p>
             </div>
-              <div v-if="profile.graduated" class="">Graduated!</div>
+            <div v-if="profile.graduated" class=""><img
+                src="src\assets\img\Vector(1).png" /> Graduated!</div>
             <div>
-              <p class="cohort">Cohort: {{profile.class}}</p>
-            </div>
-            <div>
-              <p for="email">Email: {{profile.email}}</p>
+              <p class="cohort">Cohort: {{ profile.class }}</p>
             </div>
             <div>
-              <div for="github">Github:</div>
-              <input type="url" class="form-control" v-model="editable.github" minlength="10" maxlength="100"
-                name="github" placeholder="github">
+              <p for="email">Email: {{ profile.email }}</p>
             </div>
             <div>
-              <div for="linkedin">Linkedin:</div>
-              <input type="url" class="form-control" v-model="editable.linkedin" minlength="10" maxlength="100"
-                name="linkedin" placeholder="linkedin">
+              <div for="github">Github: {{ profile.github }}</div>
             </div>
             <div>
-              <div for="picture">Picture:</div>
-              <input type="url" class="form-control" v-model="editable.picture" minlength="4" maxlength="150"
-                name="picture" placeholder="picture">
+              <div for="linkedin">Linkedin: {{ profile.linkedin }}</div>
             </div>
             <div>
-              <div for="coverImg">Cover Image:</div>
-              <input type="url" class="form-control" v-model="editable.coverImg" minlength="4" maxlength="150"
-                name="coverImg">
+              <div for="picture">Picture: {{profile.imgUrl}}</div>
             </div>
             <div>
-              <div for="bio">Bio:</div>
-              <textarea type="text" class="form-control" v-model="editable.bio" minlength="0" maxlength="800" name="bio"
-                rows="8" style="resize:none"></textarea>
+              <div for="bio">Bio: <p>{{profile.bio}}</p>
+              </div>
             </div>
             <div>
-              <div for="resume">Resume:</div>
-              <textarea type="text" class="form-control" v-model="editable.resume" minlength="0" maxlength="800"
-                name="resume" rows="8" style="resize:none"></textarea>
+              <div for="resume">Resume: <p>{{profile.resume}}</p></div>
             </div>
             <div>
             </div>
@@ -82,12 +69,13 @@ export default {
 
     const route = useRoute()
 
-    onMounted(()=> {
+    onMounted(() => {
       profilesService.getProfileById(route.params.profileId)
       postsService.getPostsByCreatorId(route.params.profileId)
     })
-    
+
     return {
+      profile: computed(()=> AppState.activeProfile),
       cover: computed(() => `url(${AppState.account?.coverImg || 'https://i.pinimg.com/originals/b5/94/6e/b5946e195cdff505e697a8dad43ae5fe.jpg'})`),
       posts: computed(() => AppState.posts),
       activePost: computed(() => AppState.activePost),
@@ -109,40 +97,48 @@ export default {
 </script>
 
 <style scoped lang="scss">
-  .left-bar {
-    height: 100vh;
-  }
+.left-bar {
+  height: 100vh;
+}
 
-  .main-profile-img {
-    height: 160px;
-    width: 160px;
-    border-radius: 50%;
-  }
+.main-profile-img {
+  height: 160px;
+  width: 160px;
+  border-radius: 50%;
+}
 
-  .posts {
-    overflow-y: auto;
-  }
+.posts {
+  overflow-y: auto;
+}
 
-  .grad-icon {
-    position: absolute;
-    right: 60px;
-    bottom: 5px;
-    width: 50px;
-    height: 50px;
-    background-color: whitesmoke;
-    border-radius: 50%;
-    border: 1px solid lightseagreen;
-  }
+.grad-icon {
+  position: absolute;
+  right: 60px;
+  bottom: 5px;
+  width: 50px;
+  height: 50px;
+  background-color: whitesmoke;
+  border-radius: 50%;
+  border: 1px solid lightseagreen;
+}
 
-  .profile-img-container {
-    position: relative;
-  }
+.profile-img-container {
+  position: relative;
+}
 
-  .mdi-l {
-    height: 2rem;
-  }
+.cover-img {
+  background-position: center;
+  background-size: cover;
+  background-image: v-bind(cover);
+}
 
-  .t-align-right {
-    text-align-last: right;
-  }
+.profile-picture {
+  background-color: aliceblue;
+  border: 1px solid aquamarine;
+  border-radius: 50%;
+}
+
+.text-shadow {
+  text-shadow: 0px 0px 3px black;
+}
 </style>
